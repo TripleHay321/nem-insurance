38cf2c000bafbfcb21b60d20444c7d0ca45ec89f
dev-phantom <godsfavour1975@gmail.com>
2023-03-20 17:48:28 +0100

    fix: used eslint to fix all auto fixable problem

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,30 +1,30 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
-  Route,
-} from 'react-router-dom'
+  Route
+} from "react-router-dom";
 
-import App from './App'
-import AdminLogin from './pages/admin/adminLogin'
-import Home from './pages/Home'
-import StaffLogin from './pages/staff/staffLogin'
-import Landing from './pages/user/landing'
-import PrintForm from './pages/user/printForm'
-import Register from './pages/user/register'
+import App from "./App";
+import AdminLogin from "./pages/admin/adminLogin";
+import Home from "./pages/Home";
+import StaffLogin from "./pages/staff/staffLogin";
+import Landing from "./pages/user/landing";
+import PrintForm from "./pages/user/printForm";
+import Register from "./pages/user/register";
 
 // creates routes and returns router object for app
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />} > 
-        <Route index element={<Home />} />
-        <Route path='/landing' element={<Landing />} />
-        <Route path='/userSignUp' element={<Register />} />
-        <Route path='/printForm' element={<PrintForm />} />
-        <Route path='/staffLogin' element={<StaffLogin />} />
-        <Route path='/adminLogin' element={<AdminLogin />} />
+    <Route path="/" element={<App />} > 
+      <Route index element={<Home />} />
+      <Route path="/landing" element={<Landing />} />
+      <Route path="/userSignUp" element={<Register />} />
+      <Route path="/printForm" element={<PrintForm />} />
+      <Route path="/staffLogin" element={<StaffLogin />} />
+      <Route path="/adminLogin" element={<AdminLogin />} />
 
     </Route>
   )
-)
-export default router
+);
+export default router;
--- src/pages/staff/staffLogin.jsx
+++ src/pages/staff/staffLogin.jsx
@@ -1,50 +1,50 @@
-import React, { useState } from 'react'
-import Input from '../../components/atoms/input'
-import AuthLayout from '../../layout/authLayout'
+import React, { useState } from "react";
+import Input from "../../components/atoms/input";
+import AuthLayout from "../../layout/authLayout";
 import { 
 
-    UserIcon, 
-    ContactIcon, 
+  UserIcon, 
+  ContactIcon 
 
-} from '../../assets'
-import { Link } from 'react-router-dom'
+} from "../../assets";
+import { Link } from "react-router-dom";
 
 export default function StaffLogin() {
-    const [fullName, setFullname] = useState("")
-    const [email, setEmail] = useState("")
-      const [remember, setRemember] = useState(true)
+  const [fullName, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [remember, setRemember] = useState(true);
 
   return (
     <div>
-        <AuthLayout
+      <AuthLayout
         heading={"Sign in as a Staff!"}
         subHeading={"Enter your username and password"}>
- <form action="" method="post"  onSubmit={(e)=>{
-                        e.preventDefault()
-                        }}>
+        <form action="" method="post"  onSubmit={(e)=>{
+          e.preventDefault();
+        }}>
 
-                        <div>
-                            <div className='w-full'>
-                                <Input className={"my-8"} label={"full name"} icon={UserIcon} type={"text"} placeholder={"Full Name"} defaultValue={fullName} onChange={e => setFullname(e.target.value)}/>
-                            </div>
-                            <div className='w-full'>
-                                <Input className={"my-8"} label={"email address"} icon={ContactIcon} type={"email"} placeholder={"Email Address"} defaultValue={email} onChange={e => setEmail(e.target.value)}/>
-                            </div>
-                                <div className='flex my-4 justify-start items-center gap-2'>
-                                <div>
-                                    <input type="checkbox" value={remember} onChange={e => setRemember(e.target.value)}/>
-                                </div>
-                                <div className='font-dmSan text-[#141414] font-400 text-md'>Remeber my password</div>
-                                </div>                              
-                            <div className='flex justify-center items-center '>
-                                <button className='bg-[#4F4F4F] w-[20rem] py-3 text-white rounded-lg '>Login</button>
-                            </div>
-                            <div className=" text-center py-4">
-                                <Link className="font-roboto text-[#828282] underline text-md font-normal">Forgot Password?</Link>
-                            </div>
-                        </div>
-                    </form>
-        </AuthLayout>
+          <div>
+            <div className="w-full">
+              <Input className={"my-8"} label={"full name"} icon={UserIcon} type={"text"} placeholder={"Full Name"} defaultValue={fullName} onChange={e => setFullname(e.target.value)}/>
+            </div>
+            <div className="w-full">
+              <Input className={"my-8"} label={"email address"} icon={ContactIcon} type={"email"} placeholder={"Email Address"} defaultValue={email} onChange={e => setEmail(e.target.value)}/>
+            </div>
+            <div className="flex my-4 justify-start items-center gap-2">
+              <div>
+                <input type="checkbox" value={remember} onChange={e => setRemember(e.target.value)}/>
+              </div>
+              <div className="font-dmSan text-[#141414] font-400 text-md">Remeber my password</div>
+            </div>                              
+            <div className="flex justify-center items-center ">
+              <button className="bg-[#4F4F4F] w-[20rem] py-3 text-white rounded-lg ">Login</button>
+            </div>
+            <div className=" text-center py-4">
+              <Link className="font-roboto text-[#828282] underline text-md font-normal">Forgot Password?</Link>
+            </div>
+          </div>
+        </form>
+      </AuthLayout>
     </div>
-  )
+  );
 }
